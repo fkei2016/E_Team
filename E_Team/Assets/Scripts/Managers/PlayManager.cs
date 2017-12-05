a86e3a4f6a2cbfff9c55a491d47a938d39527eb1
Takayoshi Ogawa <ogawa4283gamecreator>
2017-12-05 12:28:24 +0900

    [Add]仮プレイヤーのオブジェクトを作成,管理者に検索させている

--- E_Team/Assets/Scripts/Managers/PlayManager.cs
+++ E_Team/Assets/Scripts/Managers/PlayManager.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class PlayManager : SingletonMonoBehaviour<PlayManager> {
 
+    private Player[] players;
+
     protected override void Awake() {
         base.Awake();
+
+        players = FindObjectsOfType<Player>();
     }
 
     private void Update() {
