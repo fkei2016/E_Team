488c3054f7367ec31d8bd7a7dae289d1043f46ac
Fujiisan <fkei120@yahoo.co.jp>
2017-12-07 12:23:04 +0900

    パーティかる追加

---
+++ E_Team/Assets/Scripts/InputManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject CLICK_PARTICLE; // PS_TouchStarを割り当てること
+
+
+    private GameObject m_ClickParticle;
+   
+    private ParticleSystem m_ClickParticleSystem;
+
+    // Use this for initialization
+    void Start()
+    {
+        // パーティクルを生成
+        m_ClickParticle = (GameObject)Instantiate(CLICK_PARTICLE);
+
+        // パーティクルの再生停止を制御するためにコンポーネントを取得
+        m_ClickParticleSystem = m_ClickParticle.GetComponent<ParticleSystem>();
+        m_ClickParticleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // パーティクルをマウスカーソルに追従させる
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 20f;  // ※Canvasよりは手前に位置させること
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        // マウス操作
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 左ボタンダウンを検知したら、マウスカーソル位置から破裂エフェクトとキラキラエフェクトを再生する。
+            m_ClickParticle.transform.position = mousePosition;
+            m_ClickParticleSystem.Play();   // １回再生(ParticleSystemのLoopingがfalseだから)
+           
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            // 左ボタンアップを検知したら、Particleの放出を停止する
+            m_ClickParticleSystem.Stop();
+        }
+    }
+}
