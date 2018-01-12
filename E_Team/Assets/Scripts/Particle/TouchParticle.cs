6d154e88b086c0c4e34355d849b23335f6892ea0
Takayoshi Ogawa <ogawa4283gamecreator>
2018-01-12 13:39:36 +0900

    [fix]タッチイベントの関数化

--- E_Team/Assets/Scripts/Particle/TouchParticle.cs
+++ E_Team/Assets/Scripts/Particle/TouchParticle.cs
@@ -24,34 +24,31 @@ public class TouchParticle : MonoBehaviour {
 
         // パーティクルの再生停止を制御するためにコンポーネントを取得
         m_ClickParticleSystem = m_ClickParticle.GetComponent<ParticleSystem>();
-        m_ClickParticleSystem.Stop();
 
         table = new ExitGames.Client.Photon.Hashtable();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
         // マウス操作
         if (Input.GetMouseButtonDown(0))
         {
-
-            Vector3 mousePosition;
-            // パーティクルをマウスカーソルに追従させる
-            mousePosition = Input.mousePosition;
-            mousePosition.z = 20f;  // ※Canvasよりは手前に位置させること
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            //追加--
-            table.Remove("position");//テーブル内の"position"を消す
-            table.Add("position", mousePosition);//テーブル内の"position"を追加する
-
-            //PhotonViewを通して全プレイヤーにPlayTouchEventを実行させる
-            this.GetComponent<PhotonView>().RPC("PlayTouchEvent", PhotonTargets.All,table);//変数同期の時はHashTableを追加してください
-
+            Emission(Input.mousePosition, 30F);
         }
+    }
+
+    public void Emission(Vector3 worldPosition, float depth = 0F) {
+        // パーティクルをマウスカーソルに追従させる
+        var mousePosition = Camera.main.ScreenToWorldPoint(worldPosition + Vector3.forward * depth);
+
+        // テーブルに"position"を再登録
+        table.Remove("position");
+        table.Add("position", mousePosition);
+
+        //PhotonViewを通して全プレイヤーにPlayTouchEventを実行させる
+        this.GetComponent<PhotonView>().RPC("PlayTouchEvent", PhotonTargets.All, table);//変数同期の時はHashTableを追加してください
+
     }
 
     [PunRPC]
